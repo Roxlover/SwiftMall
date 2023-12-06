79d7b6bc59ec5444b88a57d26396ff3a771c3c64
SenasuDemir <ssenasudemir@gmail.com>
2023-12-06 23:10:33 +0300

    sent-email.js and Books updated

--- js/sent-email.js
+++ js/sent-email.js
@@ -1,12 +1,18 @@
 document.getElementById('subscription-form').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevents the default form submission action
 
-  var name = document.getElementById('name-input').value;
-  var email = document.getElementById('email-input').value;
+  let name = document.getElementById('name-input').value;
+  let email = document.getElementById('email-input').value;
 
   if(name && email) {
-      alert('Hello ' + name + ', we subscribed your email');
-  } else {
-      alert('Please enter both your name and email.');
+    modalContent2.innerHTML = `<p>Welcome to our community! We\'re thrilled to have you on board. You\'ve just taken a step towards staying updated with the latest and greatest from our corner.</p>`;
+
+    document.getElementById('name-input').value='';
+    document.getElementById('email-input').value='';
   }
+   else {
+    modalContent2.innerHTML = '<p>Please fill out all contact information fields.</p>';
+
+  }
+  $('#infoModal2').modal('show');
 });
