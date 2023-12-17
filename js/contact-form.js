3d0a27e4622fbe476e735732dc059060cc1633d5
tuanaulker <tuanaaulker@gmail.com>
2023-12-17 14:49:21 +0300

    Update contact-form.js

--- js/contact-form.js
+++ js/contact-form.js
@@ -7,16 +7,13 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     let message = document.getElementById('message').value;
 
     if (firstName && lastName && email && message) {
-        // alert('Hello ' + firstName + ', thank you for reaching out! We\'ll get back to you as soon as possible.');
         modalContent.innerHTML = '<p>Hello ' + firstName + ', thank you for reaching out! We\'ll get back to you as soon as possible.</p>';
 
-        //if user sends message sucessfully we can clear the form elements
         document.getElementById('first-name').value = '';
         document.getElementById('last-name').value = '';
         document.getElementById('email').value = '';
         document.getElementById('message').value = '';
     } else {
-        //alert('Please fill out all contact information fields.');
         modalContent.innerHTML = '<p>Please fill out all contact information fields.</p>';
     }
     $('#infoModal').modal('show');
